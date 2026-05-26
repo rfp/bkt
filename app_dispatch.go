517141c0ee60c69f31efccb1601aa026b0fe5e0f
Rui Patinha <rui.patinha@gmail.com>
2026-05-26 01:43:12 +0100

    Add App command dispatch methods

---
+++ app_dispatch.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func (a *App) Run(args []string) error {
+	if len(args) == 0 {
+		help()
+		return nil
+	}
+
+	switch args[0] {
+	case "auth":
+		return a.Auth(args[1:])
+	case "pr":
+		return a.PR(args[1:])
+	case "pipeline":
+		return a.Pipeline(args[1:])
+	case "help", "--help", "-h":
+		help()
+		return nil
+	default:
+		return fmt.Errorf("unknown command: %s", args[0])
+	}
+}
+
+func (a *App) Auth(args []string) error {
+	if len(args) == 0 {
+		return errors.New("missing auth subcommand")
+	}
+
+	switch args[0] {
+	case "login":
+		return a.AuthLogin()
+	case "logout":
+		if err := a.AuthLogout(); err != nil {
+			return err
+		}
+		fmt.Fprintln(a.Stdout, "Logged out")
+		return nil
+	case "status":
+		if err := ensureAuthE(a.Config); err != nil {
+			return err
+		}
+		client, err := a.Clients.New(a.Config)
+		if err != nil {
+			return err
+		}
+		u, err := client.CurrentUser()
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(a.Stdout, "Logged in as %s\nEmail: %s\nUsername: %s\nAPI: %s\n", u.DisplayName, a.Config.Email, a.Config.Username, a.Config.APIBaseURL)
+		return nil
+	default:
+		return fmt.Errorf("unknown auth subcommand: %s", args[0])
+	}
+}
+
+func (a *App) PR(args []string) error {
+	if len(args) == 0 {
+		return errors.New("missing pr subcommand")
+	}
+
+	switch args[0] {
+	case "approve":
+		repo, err := a.detectRepo("origin")
+		if err != nil {
+			return err
+		}
+		result, err := a.PRApprove(args[1:], repo)
+		if err != nil {
+			return err
+		}
+		if result.Message != "" {
+			fmt.Fprintln(a.Stdout, result.Message)
+		}
+		return nil
+	case "merge":
+		repo, err := a.detectRepo("origin")
+		if err != nil {
+			return err
+		}
+		result, err := a.PRMerge(args[1:], repo)
+		if err != nil {
+			return err
+		}
+		if result.Message != "" {
+			fmt.Fprintln(a.Stdout, result.Message)
+		}
+		return nil
+	default:
+		return fmt.Errorf("unsupported App PR subcommand: %s", args[0])
+	}
+}
+
+func (a *App) Pipeline(args []string) error {
+	if len(args) == 0 {
+		return errors.New("missing pipeline subcommand")
+	}
+
+	switch args[0] {
+	case "run":
+		repo, err := a.detectRepo("origin")
+		if err != nil {
+			return err
+		}
+		result, err := a.PipelineRun(args[1:], repo)
+		if err != nil {
+			return err
+		}
+		if result.Message != "" {
+			fmt.Fprintln(a.Stdout, result.Message)
+		}
+		return nil
+	default:
+		return fmt.Errorf("unsupported App pipeline subcommand: %s", args[0])
+	}
+}
+
+func (a *App) detectRepo(remote string) (RepoRef, error) {
+	u, err := a.Git.RemoteURL(remote)
+	if err != nil {
+		return RepoRef{}, err
+	}
+	return parseBitbucketRemoteURL(u)
+}
